51e8763b05f2a9b7a193d999ab54101f72667ab5
Eliot Howes <ejhowes@gmail.com>
2019-05-12 11:59:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |position|
-    board[position] == "X" || board[position] == "O"
+    if board[position] == "X" || board[position] == "O"
       counter += 1
-    
+    end
   end
   return counter
 end
